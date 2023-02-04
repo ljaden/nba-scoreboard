6a74dc4f366e4ef152a278a132904a63ab6f8a24
Jaden <ljaden.9@gmail.com>
2023-02-04 12:22:09 -0500

    show inactive players

--- components/Scoreboard/PlayerStats/PlayerStats.jsx
+++ components/Scoreboard/PlayerStats/PlayerStats.jsx
@@ -33,110 +33,101 @@ export default function PlayerStats({
                 <thead className="bg-white border-b">
                   <tr className="">
                     <th className=""></th>
-                    <th className=""></th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      Player
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      FG%
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      FT%
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      3P
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      PTS
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      REB
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      AST
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      STL
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      BLK
-                    </th>
-                    <th className="text-sm font-medium text-gray-900 p-2">
-                      TO
-                    </th>
+                    <th className="font-medium text-gray-900 p-2">Players</th>
+                    <th className="font-medium text-gray-900 p-2">Mins</th>
+                    <th className=" font-medium text-gray-900 p-2">FG%</th>
+                    <th className=" font-medium text-gray-900 p-2">FT%</th>
+                    <th className=" font-medium text-gray-900 p-2">3P</th>
+                    <th className=" font-medium text-gray-900 p-2">PTS</th>
+                    <th className=" font-medium text-gray-900 p-2">REB</th>
+                    <th className="font-medium text-gray-900 p-2">AST</th>
+                    <th className=" font-medium text-gray-900 p-2">STL</th>
+                    <th className=" font-medium text-gray-900 p-2">BLK</th>
+                    <th className=" font-medium text-gray-900 p-2">TO</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {players.map((player) => (
-                    <tr
-                      key={player.personId}
-                      className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-200"
-                    >
-                      <td>
-                        {player.oncourt === "1" && (
-                          <small className="font-bold">
-                            {player.jerseyNum}
-                          </small>
-                        )}
-                      </td>
-                      <td>
-                        {/* <Image */}
-                        {/*   src={`https://cdn.nba.com/headshots/nba/latest/260x190/${player.personId}.png`} */}
-                        {/*   alt="headshot" */}
-                        {/*   width={35} */}
-                        {/*   height={35} */}
-                        {/*   className="hidden sm:block w-auto h-auto" */}
-                        {/* /> */}
-                      </td>
-                      <td className="whitespace-nowrap text-sm font-medium text-gray-900">
-                        <span>{player.name}</span>
-                      </td>
-                      <td>
-                        <span>
-                          {`${player.statistics.fieldGoalsMade}/${player.statistics.fieldGoalsAttempted}`}
-                        </span>
-                        {/* <span> */}
-                        {/*   {( */}
-                        {/*     player.statistics.fieldGoalsPercentage * 100 */}
-                        {/*   ).toFixed(2)} */}
-                        {/* </span> */}
-                      </td>
-                      <td>
-                        <span>
-                          {`${player.statistics.freeThrowsMade}/${player.statistics.freeThrowsAttempted}`}
-                        </span>
-                        {/* <span> */}
-                        {/*   {player.statistics.freeThrowsPercentage.toFixed(2)} */}
-                        {/* </span> */}
-                      </td>
-                      <td>
-                        <span>{player.statistics.threePointersMade}</span>
-                      </td>
-                      <td>
-                        <span>{player.statistics.points}</span>
-                      </td>
-                      <td>
-                        <span>{player.statistics.reboundsTotal}</span>
-                      </td>
-                      <td>
-                        <span>{player.statistics.assists}</span>
-                      </td>
-                      <td>
-                        <span>{player.statistics.steals}</span>
-                      </td>
-                      <td>
-                        <span>{player.statistics.blocks}</span>
-                      </td>
-                      <td>
-                        <span>{player.statistics.turnovers}</span>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
+                  {players
+                    .filter((player) => player.status === "ACTIVE")
+                    .map((player) => (
+                      <tr
+                        key={player.personId}
+                        className="text-md bg-white border-b transition duration-300 ease-in-out hover:bg-gray-200"
+                      >
+                        <td>
+                          {player.oncourt === "1" && (
+                            <small className=" font-bold">
+                              {player.jerseyNum}
+                            </small>
+                          )}
+                        </td>
+                        <td className="whitespace-nowrap font-medium py-2">
+                          <span>{player.name}</span>
+                        </td>
+                        <td className="">
+                          <span>
+                            {parseInt(
+                              player.statistics.minutesCalculated
+                                .replace("PT", "")
+                                .replace("M", "")
+                            )}
+                          </span>
+                        </td>
+                        <td>
+                          <span>
+                            {`${player.statistics.fieldGoalsMade}/${player.statistics.fieldGoalsAttempted}`}
+                          </span>
+                          {/* <span> */}
+                          {/*   {( */}
+                          {/*     player.statistics.fieldGoalsPercentage * 100 */}
+                          {/*   ).toFixed(2)} */}
+                          {/* </span> */}
+                        </td>
+                        <td>
+                          <span>
+                            {`${player.statistics.freeThrowsMade}/${player.statistics.freeThrowsAttempted}`}
+                          </span>
+                          {/* <span> */}
+                          {/*   {player.statistics.freeThrowsPercentage.toFixed(2)} */}
+                          {/* </span> */}
+                        </td>
+                        <td>
+                          <span>{player.statistics.threePointersMade}</span>
+                        </td>
+                        <td>
+                          <span>{player.statistics.points}</span>
+                        </td>
+                        <td>
+                          <span>{player.statistics.reboundsTotal}</span>
+                        </td>
+                        <td>
+                          <span>{player.statistics.assists}</span>
+                        </td>
+                        <td>
+                          <span>{player.statistics.steals}</span>
+                        </td>
+                        <td>
+                          <span>{player.statistics.blocks}</span>
+                        </td>
+                        <td>
+                          <span>{player.statistics.turnovers}</span>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>{" "}
               </table>
             </div>
           </div>
         </div>
+      </div>
+      <div>
+        <small>Inactive: </small>
+        {players
+          .filter((player) => player.status === "INACTIVE")
+          .map((player) => player.nameI)
+          .map((name) => (
+            <small className="text-gray-400">{name}, </small>
+          ))}
       </div>
     </>
   );
